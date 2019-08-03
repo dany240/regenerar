e5b329a1c28956b3ce58fc656758e5a097151b13
mario_morales <mmariohernandez58@gmail.com>
2019-08-03 14:00:04 -0500

    crud de materias, grados y periodos

--- proyecto_final/materias_demas/urls.py
+++ proyecto_final/materias_demas/urls.py
@@ -7,8 +7,8 @@ from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns=[
     #url(),
-    url(r'^cargar_materia',materias,name='materia'), #como se realza una url
-    url(r'^cargar_grado',grados,name='grado'),
+    url(r'^cargar_materia',cargar_materias,name='materia'), #como se realza una url
+    url(r'^cargar_grado',cargar_grados,name='grado'),
     url(r'^cargar_periodo',cargar_periodo,name='periodo'),
     url(r'^listar_periodo',listar_periodo, name='list_period'),
     url(r'^listar_grado',listar_grado, name='list_grad'),
@@ -16,4 +16,7 @@ urlpatterns=[
     url(r'^actualizar_periodo/(?P<id_periodo>\d+)$',periodo_actualizar, name ='actual_period'),
     url(r'^actualizar_grado/(?P<id_grado>\d+)$',grado_actualizar, name ='actual_grad'),
     url(r'^actualizar_materia/(?P<id_materia>\d+)$',materia_actualizar, name ='actual_mater'),
+    url(r'^eliminar_periodo/(?P<id_periodo>\d+)$',eliminar_periodo, name ='eliminar_period'),
+    url(r'^eliminar_materia/(?P<id_materia>\d+)$',eliminar_materia, name ='eliminar_mater'),
+    url(r'^eliminar_grado/(?P<id_grado>\d+)$',eliminar_grado, name ='eliminar_grad'),
 ]
--- proyecto_final/materias_demas/views.py
+++ proyecto_final/materias_demas/views.py
@@ -13,14 +13,13 @@ from .models import  *
 
 # Create your views here.
 
-def materias(request):
+def cargar_materias(request):
     formas = None
     if (request.method == 'POST'):
         formas = formas_entrada_materias(request.POST)
         if (formas.is_valid()):
             formas.save()
-            return HttpResponse('<h1> Materias guardadas </h1>')
-
+            return redirect('list_mater')
     else:
         formas = formas_entrada_materias
         return render(request, template_name='html/Create/'
@@ -29,14 +28,14 @@ def materias(request):
                           'formas': formas
                       })
 
-def grados(request):
+
+def cargar_grados(request):
     formas = formas_entrada_grados
     if (request.method == 'POST'):
         formas = formas_entrada_grados(request.POST)
         if (formas.is_valid()):
             formas.save()
-            return HttpResponse('<h1> Grados guardados </h1>')
-
+            return redirect('list_grad')
     else:
         formas = formas_entrada_grados
         return render(request, template_name='html/Create/'
@@ -45,14 +44,14 @@ def grados(request):
                           'formas': formas
                       })
 
+
 def cargar_periodo (request):
     formas = None
     if (request.method == 'POST'):
         formas = formas_entrada_periodo(request.POST)
         if (formas.is_valid()):
             formas.save()
-            return HttpResponse('<h1> Periodos Guardados </h1>')
-
+            return redirect('list_period')
     else:
         formas = formas_entrada_periodo
         return render(request, template_name='html/Create/'
@@ -60,6 +59,8 @@ def cargar_periodo (request):
                       context={
                           'formas': formas
                       })
+
+
 def periodo_actualizar(request:HttpRequest,id_periodo):
     periodo_ant= periodo.objects.get(id_periodo=id_periodo)
     forma=formas_entrada_periodo(instance=periodo_ant)#passando informacion al form
@@ -67,7 +68,7 @@ def periodo_actualizar(request:HttpRequest,id_periodo):
         forma=formas_entrada_periodo(request.POST,instance=periodo_ant)
         if(forma.is_valid()):
             forma.save()
-            return HttpResponse('<h1> Periodo Actualizado </h1>')
+            return redirect('list_period')
         else:
             messages.add_message(request)
     else:
@@ -77,6 +78,7 @@ def periodo_actualizar(request:HttpRequest,id_periodo):
                       context={
                           'formas': forma
                       })
+
 
 def grado_actualizar(request:HttpRequest, id_grado):
     grado_ant = grado.objects.get(id_grado=id_grado)
@@ -85,7 +87,7 @@ def grado_actualizar(request:HttpRequest, id_grado):
         forma=formas_entrada_grados(request.POST, instance=grado_ant)
         if(forma.is_valid()):
             forma.save()
-            return redirect( 'list_grad')
+            return redirect('list_grad')
         else:
             messages.add_message(request)
     else:
@@ -95,6 +97,8 @@ def grado_actualizar(request:HttpRequest, id_grado):
                       context={
                           'formas': forma
                       })
+
+
 
 def materia_actualizar(request:HttpRequest, id_materia):
     materia_ant = materia.objects.get(id_materia=id_materia)
@@ -103,7 +107,7 @@ def materia_actualizar(request:HttpRequest, id_materia):
         forma=formas_entrada_materias(request.POST, instance=materia_ant)
         if(forma.is_valid()):
             forma.save()
-            return HttpResponse('<h1> Materia actualizada <h1>')
+            return redirect('list_mater')
         else:
             messages.add_message(request)
     else:
@@ -114,6 +118,46 @@ def materia_actualizar(request:HttpRequest, id_materia):
                           'formas': forma
                       })
 
+
+def eliminar_periodo(request:HttpRequest, id_periodo):
+    period = periodo.objects.get(id_periodo=id_periodo)
+    if (request.method == 'POST'):
+        period.delete()
+        return redirect('list_period')
+    else:
+        return render(request,
+                      template_name='html/delete_m/'
+                                    'delete_m.html',
+                      context={'forma': period}
+                      )
+
+
+def eliminar_materia(request:HttpRequest, id_materia):
+    mater = materia.objects.get(id_materia=id_materia)
+    if (request.method == 'POST'):
+        mater.delete()
+        return redirect('list_mater')
+    else:
+        return render(request,
+                      template_name='html/delete_m/'
+                                    'delete_m.html',
+                      context = {'forma': mater}
+                      )
+
+
+def eliminar_grado(request:HttpRequest, id_grado):
+    grad = grado.objects.get(id_grado=id_grado)
+    if (request.method == 'POST'):
+        grad.delete()
+        return redirect('list_grad')
+    else:
+        return render(request,
+                      template_name='html/delete_m/'
+                                    'delete_m.html',
+                      context={'forma': grad}
+                      )
+
+
 def grado_estudiantes_insertar(request):
     pass
 
@@ -121,10 +165,13 @@ def listar_periodo(request):
     period = periodo.objects.all()
     return render(request, template_name='html/Listar_m/Listar_periodo.html', context={'forma': period})
 
+
 def listar_grado(request):
     grad = grado.objects.all()
     return render(request, template_name='html/Listar_m/listar_grado.html', context={'forma': grad})
 
+
 def listar_materia(request):
     mater = materia.objects.all()
     return render(request, template_name='html/Listar_m/listar_materia.html', context={'forma': mater})
+
